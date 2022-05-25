23b1c50e03d5bfc905f90b6f989800fd3df2fc5e
kirill <ischencko.kir@gmail.com>
2022-05-25 11:58:14 +0300

    Создает запрос для получения списка книг

--- webapp/app.py
+++ webapp/app.py
@@ -1,7 +1,14 @@
 from bumbo.api import API
 
+from storage import BookStorage
+
+
 app = API()
+book_storage = BookStorage()
+book_storage.create(name="7 habits of highly effective people", author="Stephen Covey")
+
 
 @app.route("/", allowed_methods=["get"])
 def index(req, resp):
-    resp.html = app.template("index.html")
+    books = book_storage.all()
+    resp.html = app.template("index.html", context={"books": books})
