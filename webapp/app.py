63e299ce2b2d68ebc678cac8c1967e14c14f382e
kirill <ischencko.kir@gmail.com>
2022-05-25 12:04:07 +0300

    Добавляет роут login

--- webapp/app.py
+++ webapp/app.py
@@ -1,11 +1,15 @@
 from bumbo.api import API
-
 from storage import BookStorage
+from auth import STATIC_TOKEN
 
 
 app = API()
 book_storage = BookStorage()
 book_storage.create(name="7 habits of highly effective people", author="Stephen Covey")
+
+@app.route("/login", allowed_methods=["post"])
+def login(req, resp):
+    resp.json = {"token": STATIC_TOKEN}
 
 
 @app.route("/", allowed_methods=["get"])
