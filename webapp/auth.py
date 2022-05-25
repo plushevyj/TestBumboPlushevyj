1ccd6f125583b96da32b730cf41d84e2840962ec
kirill <ischencko.kir@gmail.com>
2022-05-25 12:06:01 +0300

    Создает декоратор login_required

--- webapp/auth.py
+++ webapp/auth.py
@@ -1,1 +1,20 @@
+import re
+from bumbo.middleware import Middleware
+
 STATIC_TOKEN = "ae4CMvqBe2"
+
+
+class InvalidTokenException(Exception):
+    pass
+
+
+def login_required(handler):
+    def wrapped_view(request, response, *args, **kwargs):
+        token = getattr(request, "token", None)
+
+        if token is None or not token == STATIC_TOKEN:
+            raise InvalidTokenException("Invalid Token")
+
+        return handler(request, response, *args, **kwargs)
+
+    return wrapped_view
